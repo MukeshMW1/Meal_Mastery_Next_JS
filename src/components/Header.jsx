7e8aff07f7cab7a22d305c439b60e73b8b39088c
MukeshMW1 <142603749+MukeshMW1@users.noreply.github.com>
2024-07-23 10:03:05 +0545

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import {React} from 'react'
 export const Header=()=>{
     return(
         <div className="mt-[40px] flex items-center justify-center mb-4 gap-4  text-secondary-light">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTObbxnTRARq3vIAr_z3Iks4bqVkMsuck7eRw&s" alt="Chef Icon" className="mr-2 rounded-[50%] cursor-pointer lg:h-[50px] lg:w-[50px] sm:h-[20px] sm:w-[20px]" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTObbxnTRARq3vIAr_z3Iks4bqVkMsuck7eRw&s" alt="Chef Icon" className="mr-2 rounded-[50%] cursor-pointer lg:h-[50px] lg:w-[50px] h-[20px] w-[20px]" />
 
 
 

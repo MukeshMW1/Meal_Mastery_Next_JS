73b72b9b964b6ac29170b44d5e3a04e8cdd303d0
MukeshMW1 <MukeshBhattaMPB111@gmail.com>
2025-02-19 13:21:48 +0545

    UI Changes

--- Meal_Mastery_Next_JS/src/components/Header.jsx
+++ Meal_Mastery_Next_JS/src/components/Header.jsx
@@ -1,16 +1,69 @@
 import {React} from 'react'
+import Logo from './Logo'
 
 export const Header=()=>{
     return(
-        <div className="mt-[40px] flex items-center justify-center mb-4 gap-4  text-secondary-light">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTObbxnTRARq3vIAr_z3Iks4bqVkMsuck7eRw&s" alt="Chef Icon" className="mr-2 rounded-[50%] cursor-pointer lg:h-[50px] lg:w-[50px] h-[20px] w-[20px]" />
+<header>
+    <div class="relative bg-black">
+        <div class="absolute inset-0">
+            <img class="object-cover w-full h-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/headers/3/coworking-space.jpg" alt="" />
+        </div>
 
+        <div class="absolute inset-0 bg-black/30"></div>
 
+        <div class="relative px-4 mx-auto sm:px-6 lg:px-8">
+            <nav class="flex items-center justify-between h-16 lg:h-20">
+                <div class="flex-shrink-0">
+                    <a href="#" title="" class="flex">
+                       <Logo/>
+                    </a>
+                </div>
 
-        <h1 className="lg:text-[18px] sm:text-[12px] font-bold">Generate Food Recipe with AI</h1>
+                <button type="button" class="inline-flex p-2 text-white transition-all duration-200 rounded-md lg:hidden focus:bg-gray-800 hover:bg-gray-800">
+                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                    </svg>
+                </button>
 
+                <div class="hidden lg:flex lg:items-center lg:space-x-10">
+                    <a href="#" title="" class="text-base font-medium text-white"> Features </a>
 
+                    <a href="#" title="" class="text-base font-medium text-white"> Solutions </a>
 
-      </div>
+                    <a href="#" title="" class="text-base font-medium text-white"> Resources </a>
+
+                    <a href="#" title="" class="text-base font-medium text-white"> Pricing </a>
+                </div>
+
+                <a href="#" title="" class="items-center justify-center hidden px-6 py-3 text-base font-semibold text-black transition-all duration-200 bg-yellow-400 border border-transparent rounded-full lg:inline-flex hover:bg-yellow-500 focus:bg-yellow-500" role="button"> Join Now </a>
+            </nav>
+        </div>
+    </div>
+
+    <nav class="flex flex-col justify-between w-full max-w-xs min-h-screen px-4 py-10 bg-black sm:px-6 lg:hidden">
+        <button type="button" class="inline-flex p-2 text-white transition-all duration-200 rounded-md focus:bg-gray-800 hover:bg-gray-800">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+
+        <div class="flex flex-col flex-grow h-full">
+            <nav class="flex flex-col flex-1 mt-10 space-y-2">
+                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Features </a>
+
+                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Solutions </a>
+
+                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Resources </a>
+
+                <a href="#" title="" class="flex w-full py-2 font-medium text-white transition-all duration-200 focus:text-opacity-70"> Pricing </a>
+            </nav>
+
+            <div class="flex flex-col items-start">
+                <a href="#" title="" class="inline-flex items-center justify-center w-auto px-6 py-3 mt-auto text-base font-semibold text-black transition-all duration-200 bg-yellow-400 border border-transparent rounded-full hover:bg-yellow-500 focus:bg-yellow-500" role="button"> Join Now </a>
+            </div>
+        </div>
+    </nav>
+</header>
+
     )
 }
---
+++ Meal_Mastery_Next_JS/src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="text-center">
+      <div>
+        <img src="./MM.png" className=' w-50 h-40' />
+      </div>
+     
+   
+    </div>
+  );
+};
+
+export default Logo;
